67b6bdf9250b571f59ab180d4312257783fa7386
jlo <junior.lonkong@afreetech.com>
2025-07-25 17:59:55 +0100

    sdfs

--- config/settings.py
+++ config/settings.py
@@ -177,4 +177,4 @@ LOGGING = {
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / "static"]
-STATIC_ROOT = BASE_DIR / "staticfiles"
+# STATIC_ROOT = BASE_DIR / "staticfiles"
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./templates/**/*.html",
+    "./**/*.py",
+    "./tailwind_src/**/*.css",
+    "./static/js/**/*.js",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
