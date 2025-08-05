db85b80a2cc415cbe5889df5640fb4a7a56fb5f1
Ndissok <145775138+Ndissok@users.noreply.github.com>
2025-08-05 21:07:11 +0100

    updated the host and port. post point to service db

--- config/settings.py
+++ config/settings.py
@@ -98,8 +98,8 @@ DATABASES = {
         'NAME': os.environ.get("POSTGRES_DB"),
         'USER': os.environ.get("POSTGRES_USER"),
         'PASSWORD': os.environ.get("POSTGRES_PASSWORD"),
-        'HOST': os.environ.get("POSTGRES_HOST"),
-        'PORT': os.environ.get("POSTGRES_PORT"),
+        'HOST': 'db',
+        'PORT': '5432',
     }
 }
 
